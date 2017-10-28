19d8c858f908e237efc45fdc4526d8dc77c95f21
Joel Stewart <joel@stewbeans.com>
2017-10-28 17:38:46 -0400

    add template, convert int types

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # elastic_sw_indexer
-Simple project to index data at SWAPI to an Elasticsearch using Python
+
+The Python-Elasticsearch lab at https://tryolabs.com/blog/2015/02/17/python-elasticsearch-first-steps/ provides a basic introduction into using Python to index data from https://swapi.co/  into Elasticsearch.   
+
+This project takes that demonstration a few more steps.  The objective is to learn more about Elasticsearch mappings, search, and analytics.   
+
+
+# prerequisites
+
+ - Elasticsearch >= 2.4 
+ - Python 2.7, with pip and virtualenv
+
+## basic setup
+
+```bash
+$ git clone https://github.com/joelstewart/elastic_sw_indexer.git
+$ virturalenv swesenv
+$ source swesenv/bin/activate
+$ cd elastic_sw_indexer
+(swesenv)$ pip install -r requirements.txt
+```
+
+Edit the configs in indexsw.py for your environment.
+
+Install the template in elasticsearch:
+```bash
+$ curl -XPUT http://elasticsearch:9200/_template/sw_template -d@template.json
+```
+
+Run the indexer.   This will add 6 indexes to Elasticsearch for the sw data: sw_people, sw_planets, sw_starships, sw_films, sw_vehicles, sw_species.
+
+```bash
+(swesenv)$ python indexsw.py
+``` 
+
+
+# the index template 
+The index template will turns off analysis for all string fields, except for the opening crawl text field in the sw_film type.   
+
+# fixing the data
+The sw data contains many fields in json that are strings that are numeric values.  The template could define those types as integer, and elasticsearch would be smart enough to know how to convert those strings to integers.   Instead, the indexing script takes care of changing types of fields.
+
+# example queries
+
+## full text queries
+
+From a browser, test a few queries
+
+http://192.168.56.10:9200/sw*/_search?q=space&pretty
+
+The _all field is enabled (https://www.elastic.co/guide/en/elasticsearch/reference/2.4/mapping-all-field.html), so this query has higher relevance for the "shortest" json containing the word space as a value of one of its fields.
+
+
+
+
+ 
+# more to come?  
+
+Explore using Sirenjoin to perform joins https://github.com/sirensolutions/siren-join
+
+
--- indexsw.py
+++ indexsw.py
@@ -1,24 +1,82 @@
 import requests
 from elasticsearch import Elasticsearch
 import json
+import sys
 
 #configs
 host="192.168.56.10"
 port=9200
 esrest='http://'+host+':'+str(port)
+
+#template mapping to use
+
+
+#separate indexes or same index.
+separate=True
+
+#log data as it is added
 verbose=True
-separate=False
+
+
+#returns the int value as id from a url 
+def idFromUrl(url):
+  tokens= url.split("/")
+  return int(tokens[len(tokens) - 2])
+
+#returns in for string, or None
+def asint(s):
+  try:
+    return int(s)     
+  except:
+    return None  
+
+#modifies swapi json to replace some strings as ints
+def format(j):
+  if 'height' in j:
+    j['height'] = asint(j['height'])
+  if 'mass' in j:
+    j['mass'] = asint(j['mass'])
+  if 'diameter' in j:
+    j['diameter'] = asint(j['diameter'])
+  if 'population' in j:
+    j['population'] = asint(j['population'])
+  if 'orbital_period' in j:
+    j['orbital_period'] = asint(j['orbital_period'])
+  if 'surface_water' in j:
+    j['surface_water'] = asint(j['surface_water'])
+  if 'rotation_period' in j:
+    j['rotation_period'] = asint(j['rotation_period'])
+  if 'passengers' in j:
+    j['passengers'] = asint(j['passengers'])
+  if 'cargo_capacity' in j:
+    j['cargo_capacity'] = asint(j['cargo_capacity'])
+  if 'max_atmosphering_speed' in j:
+    j['max_atmosphering_speed'] = asint(j['max_atmosphering_speed'])
+  if 'diameter' in j:
+    j['diameter'] = asint(j['diameter'])
+  if 'crew' in j:
+    j['crew'] = asint(j['crew'])
+  if 'length' in j:
+    j['length'] = asint(j['length'])
+  if 'MGLT' in j:
+    j['MGLT'] = asint(j['MGLT'])
+  if 'cost_in_credits' in j:
+    j['cost_in_credits'] = asint(j['cost_in_credits'])
+  if 'average_lifespan' in j:
+    j['average_lifespan'] = asint(j['average_lifespan'])
+  if 'average_height' in j:
+    j['average_height'] = asint(j['average_height'])
 
 #iterates through pages of sw stuff and indexes it
 def idxsw(indx, type, swapi):
   r=requests.get(swapi+'?page=1')
   while r.status_code == 200:
     j = json.loads(r.content)
-    for obj in j['results']:
-      id = obj['url']
+    for obj in j['results']:      
+      id = idFromUrl(obj['url'])
+      format(obj)
       if verbose:
 	print obj
-      print id
       es.index(index=indx, doc_type=type, id=id, body=obj)
     if 'None' !=  str(j['next']):
        r = requests.get(j['next'])
@@ -30,7 +88,7 @@ res = requests.get(esrest)
 print(res.content)
 es = Elasticsearch([{'host': host, 'port': port}])
 
-#index into separate indexes...  
+#index   
 if (separate):
   idxsw('sw_people','people','http://swapi.co/api/people/')
   idxsw('sw_species','species','http://swapi.co/api/species/')
